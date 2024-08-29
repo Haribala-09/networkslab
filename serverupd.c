235918943f140b850805dc9e59ebee070e53055b
zeatoen <s_i_am-r@outlook.com>
2024-08-29 07:12:53 +0000

    fnh

--- serverupd.c
+++ serverupd.c
@@ -1,26 +1,31 @@
-#include<stdio.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<sys/tpes.h>
-#include<string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
 
+int main()
+{
+	char read_buffer[100], write_buffer[100];
+	int listenfd, len;
+	struct sockaddr_in servaddr,cliaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
 
+	listenfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-int main(){
-	char read_buffer[100],write_buffer[100];
-	int listenfd,len;
-	struct sockaddr_in servaddr;
-	memset(&servaddr,0,sizeof(servaddr));
+	// Creating a servaddr of type ipv4 and port 3200 with taking any inbuild ip as specified as INADDR_ANY
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(5000);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	listenfd=socket(AF_INET,SOCK_DGRAM,0);
+	bind(listenfd, (struct sockaddr *)&servaddr, sizeof servaddr);
 
-	//Creating a servaddr of type ipv4 and port 3200 with taking any inbuild ip as specified as INADDR_ANY
-	servaddr.sin_family=AF_INET;
-	servaddr.sin_port=htons(3200);
-	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	len = sizeof(cliaddr);
+	int n = recvfrom(listenfd, read_buffer, sizeof(read_buffer),
+					 0, (struct sockaddr *)&cliaddr, &len); // receive message from server
+	read_buffer[n] = '\0';
+	puts(read_buffer);
 
-	bind(listenfd,(struct sockaddr*)&servaddr,sizeof servaddr);
-
-
-
+	sendto(listenfd, "This life sucks!", sizeof(write_buffer), 0,
+		   (struct sockaddr *)&cliaddr, sizeof(cliaddr));
 }
